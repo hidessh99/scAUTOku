55bb41d7e36aa5ed48b54a50267583bf218e881f
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2025-08-29 17:40:44 +0700

    q

--- package-gohide.sh
+++ package-gohide.sh
@@ -36,8 +36,9 @@ echo -e "${blue}─────────────────────�
 echo -e "${green} INSTALLASI Check  package HIdeSSH      ${neutral}"
 echo -e "${blue}─────────────────────────────────────────${neutral}"
 cd
-wget -q -O /usr/local/bin/check-vless "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addvless" && chmod +x /usr/local/bin/add-vless
-wget -q -O /usr/local/bin/check-trojan "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addtrojan" && chmod +x /usr/local/bin/add-trojan
-wget -q -O /usr/local/bin/check-shadowsocks "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addshadowsocks" && chmod +x /usr/local/bin/add-shadowsocks
-wget -q -O /usr/local/bin/check-ssh "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addssh" && chmod +x /usr/local/bin/add-ssh
-wget -q -O /usr/local/bin/check-vmess "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/checkuservmess.sh" && chmod +x /usr/local/bin/add-vmess        
+# wget -q -O /usr/local/bin/check-vless "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addvless" && chmod +x /usr/local/bin/add-vless
+# wget -q -O /usr/local/bin/check-trojan "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addtrojan" && chmod +x /usr/local/bin/add-trojan
+# wget -q -O /usr/local/bin/check-shadowsocks "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addshadowsocks" && chmod +x /usr/local/bin/add-shadowsocks    
+# wget -q -O /usr/local/bin/check-ssh "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addssh" && chmod +x /usr/local/bin/add-ssh
+
+wget -q -O /usr/local/bin/check-vmess "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/checkuservmess.sh" && chmod +x /usr/local/bin/check-vmess      
