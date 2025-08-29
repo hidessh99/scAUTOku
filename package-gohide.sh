3faadff0a1353e04cab2c5b7711ad09c8ad6a9e0
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2025-08-29 17:39:35 +0700

    a

--- package-gohide.sh
+++ package-gohide.sh
@@ -17,8 +17,7 @@ wget -q -O /usr/local/bin/add-vless "https://raw.githubusercontent.com/hidessh99
 wget -q -O /usr/local/bin/add-trojan "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addtrojan" && chmod +x /usr/local/bin/add-trojan
 wget -q -O /usr/local/bin/add-shadowsocks "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addshadowsocks" && chmod +x /usr/local/bin/add-shadowsocks
 wget -q -O /usr/local/bin/add-ssh "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addssh" && chmod +x /usr/local/bin/add-ssh
-
-
+clear
 
 echo -e "${blue}─────────────────────────────────────────${neutral}"
 echo -e "${green} INSTALLASI delete package HIdeSSH      ${neutral}"
@@ -29,4 +28,16 @@ wget -q -O /usr/local/bin/del-trojan "https://raw.githubusercontent.com/hidessh9
 wget -q -O /usr/local/bin/del-vless "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/dellaccvless.sh" && chmod +x /usr/local/bin/del-vless
 wget -q -O /usr/local/bin/del-addshadowsocks "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/dellaccshadowsocks.sh" && chmod +x /usr/local/bin/del-addshadowsocks
 wget -q -O /usr/local/bin/del-ssh "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/dellaccssh.sh" && chmod +x /usr/local/bin/del-ssh
+clear
 
+
+
+echo -e "${blue}─────────────────────────────────────────${neutral}"
+echo -e "${green} INSTALLASI Check  package HIdeSSH      ${neutral}"
+echo -e "${blue}─────────────────────────────────────────${neutral}"
+cd
+wget -q -O /usr/local/bin/check-vless "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addvless" && chmod +x /usr/local/bin/add-vless
+wget -q -O /usr/local/bin/check-trojan "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addtrojan" && chmod +x /usr/local/bin/add-trojan
+wget -q -O /usr/local/bin/check-shadowsocks "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addshadowsocks" && chmod +x /usr/local/bin/add-shadowsocks
+wget -q -O /usr/local/bin/check-ssh "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/addssh" && chmod +x /usr/local/bin/add-ssh
+wget -q -O /usr/local/bin/check-vmess "https://raw.githubusercontent.com/hidessh99/scAUTOku/refs/heads/main/project/checkuservmess.sh" && chmod +x /usr/local/bin/add-vmess        
