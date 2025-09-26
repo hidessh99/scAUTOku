dad82e534b9669d0b2b1a2d06a1ffeda315f6fdf
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2025-09-26 20:47:27 +0700

    aad

--- golang/rest-go.sh
+++ golang/rest-go.sh
@@ -34,7 +34,7 @@ cat >>/etc/nginx/conf.d/xray.conf <<NGINX_EOF
 
 # VPN API HTTPS Configuration
 server {
-    listen ${port} ssl http2 reuseport;
+    listen 3000 ssl http2 reuseport;
     ssl_certificate /etc/xray/xray.crt;
     ssl_certificate_key /etc/xray/xray.key;
     ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
