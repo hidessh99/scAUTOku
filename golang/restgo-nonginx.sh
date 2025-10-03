fd93c548e9c2f957b3ea533982b799eae7ce098d
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2025-10-03 09:35:53 +0700

    tanpa

---
+++ golang/restgo-nonginx.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
+clear
+
+echo -e "${blue}─────────────────────────────────────────${neutral}"
+echo -e "${green}   INSTALLASI rest-go HIdeSSH       ${neutral}"
+echo -e "${blue}─────────────────────────────────────────${neutral}"
+cd
+
+
+echo -e "${blue}─────────────────────────────────────────${neutral}"
+echo -e "${green}   INSTALLASI golang restapi HIdeSSH       ${neutral}"
+echo -e "${blue}─────────────────────────────────────────${neutral}"
+
+cd
+wget -q -O /usr/local/bin/vpn-api "https://github.com/hidessh99/scAUTOku/raw/refs/heads/main/golang/vpn-rest" && chmod +x /usr/local/bin/vpn-api
+
+clear
+read -p "Port example 3005 : " port
+uuid=$(cat /proc/sys/kernel/random/uuid)
+
+
+
+cd
+chmod 644 /etc/systemd/system/vpn-api.service
+
+sudo systemctl daemon-reload
+sudo systemctl start vpn-api
+sudo systemctl enable vpn-api
+
+echo "Installation completed"
+echo "save all data below"
+echo "You can access key header Bearer: ${uuid}"
+echo "Port Rest Api: ${port}"
