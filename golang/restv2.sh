60d5b5b32057e803dbb5ef42b5c5e7216e544a9c
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2025-10-07 10:13:52 +0700

    fs

---
+++ golang/restv2.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
+clear
+
+echo -e "${blue}─────────────────────────────────────────${neutral}"
+echo -e "${green}   INSTALLASI rest-go HIdeSSH       ${neutral}"
+echo -e "${blue}─────────────────────────────────────────${neutral}"
+cd
+
+
+echo -e "${blue}─────────────────────────────────────────${neutral}"
+echo -e "${green}   INSTALLASI golang restapi HIdeSSH       ${neutral}"
+echo -e "${blue}─────────────────────────────────────────${neutral}"
+
+cd
+wget -q -O /usr/local/bin/vpn-api "https://github.com/hidessh99/scAUTOku/raw/refs/heads/main/golang/vpn-apiv2" && chmod +x /usr/local/bin/vpn-api
+
+clear
+read -p "Port example 3005 : " port
+uuid=$(cat /proc/sys/kernel/random/uuid)
+
+
+cat >/etc/systemd/system/vpn-api.service <<EOF
+[Unit]
+Description=VPN Account Management API
+After=network.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/local/bin/vpn-api
+Restart=always
+RestartSec=10
+Environment=PORT=${port}
+Environment=API_KEY=${uuid}
+Environment=AllowOrigins=*
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+cd
+chmod 644 /etc/systemd/system/vpn-api.service
+
+sudo systemctl daemon-reload
+sudo systemctl start vpn-api
+sudo systemctl enable vpn-api
+sudo systemctl restart vpn-api
+
+echo "Installation completed"
+echo "save all data below"
+echo "You can access key header Bearer: ${uuid}"
+echo "Port Rest Api: ${port}"
